3fc577657036868ce915bccbde0c4b896376af45
LauraStru <40288909+LauraStru@users.noreply.github.com>
2018-07-17 14:29:29 +0200

    Add files via upload
    Noch nicht die endgültige Fassung!!! ;)

---
+++ Flugbahn.cpp
@@ -0,0 +1,147 @@
+/*
+ * Flugbahn.cpp
+ *
+ *  Created on: 16.07.2018
+ *      Author: ls
+ */
+
+#include "Flugbahn.h"
+
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+
+using namespace std;
+
+/* function prototypes */
+double dnx(double, double [], double [], int );
+double rkv4(double(*)(double, double [], double [], int ),
+               double, double, double [], double [], int);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*==============================================================
+  System of first order differential equations for the RK solver
+  For a system of n first-order ODEs
+  x [] array - x values
+  dx[] array - dx/dt values
+  For a system of n/2 second order ODEs follow the agreement
+  In:  x[] array
+  # first n/2 elements are x
+  # last  n/2 elements are dx/dt
+  Out: dx[] array
+  # first n/2 elements are first order derivatives  (or dx/dt)
+  # last  n/2 elements are second order derivatives (or d2x/dt2)
+  example: 2D projectile motion in (x,y) plane
+  In           Out
+  x[0] - x     dx[0] - x'
+  x[1] - y     dx[1] - y'
+  x[2] - x'    dx[2] - x"
+  x[3] - y'    dx[3] - y"
+==============================================================*/
+
+
+// Muss noch geändert werden!!!!!!!!!!:
+
+ double dnx(double t, double r[], double dr[], int n)
+{
+/* first order */
+    dr[0] = r[2];
+    dr[1] = r[3];
+/* second order */
+    //dr[2] = r[2]*t+(-1.0)*(G*M/(pow(r[0],2)+pow(r[1],2)))*cos((atan(r[1]/r[0])*rad)+(1/2)*(c_w/m)*A*rho*pow(r[2]*cos((atan(r[1]/r[0]))*rad), 2));				//zweite Ableitungen (r und phi)
+    //dr[3] = r[3]*t+(-1.0)*(G*M/(pow(r[0],2)+pow(r[1],2)))*sin((atan(r[1]/r[0])*rad)+(1/2)*(c_w/m)*A*rho*pow(r[3]*sin((atan(r[1]/r[0]))*rad), 2));
+
+    //dr[2] = (-1.0)*G*M/pow(sqrt((pow(r[0],2)+pow(r[1],2))),3)*r[0]-(1/2)*(c_w)*A*rho*pow(r[2], 2);				//zweite Ableitungen (r und phi)
+    //dr[3] = (-1.0)*G*M/pow(sqrt((pow(r[0],2)+pow(r[1],2))),3)*r[1]-(1/2)*(c_w)*A*rho*pow(r[3], 2);
+
+    dr[2] = (-1.0)*G*M/pow(sqrt((pow(r[0],2)+pow(r[1],2))),3)*r[0]-(1/2)*(c_w)*A*(rho0*exp(-(pow(r[0],2)+pow(r[1],2))/H0))*pow(r[2], 2);				//zweite Ableitungen (r und phi)
+    dr[3] = (-1.0)*G*M/pow(sqrt((pow(r[0],2)+pow(r[1],2))),3)*r[1]-(1/2)*(c_w)*A*(rho0*exp(-(pow(r[0],2)+pow(r[1],2))/H0))*pow(r[3], 2);
+
+
+    //dr[2] = ((-1.0)*G*M/(pow(r[0],2)+pow(r[1],2)))*cos((atan(r[1]/r[0])*rad))-(1/2)*(c_w/m)*A*rho*pow(r[2]*cos((atan(r[1]/r[0]))*rad), 2);				//zweite Ableitungen (r und phi)
+    //dr[3] = ((-1.0)*G*M/(pow(r[0],2)+pow(r[1],2)))*sin((atan(r[1]/r[0])*rad))-(1/2)*(c_w/m)*A*rho*pow(r[3]*sin((atan(r[1]/r[0]))*rad), 2);
+
+    //printf("%f ",atan(r[1]/r[0])*rad);
+
+    return 0.0;
+}
+
+
+
+/*==========================================================
+ rk4_dn1.cpp: Solution of a system of n first-order ODE
+ method:      Runge-Kutta 4th-order
+ written by: Alex Godunov
+ last revision: 7 October 2009
+----------------------------------------------------------
+ call ...
+ dnx(t,x[],dx[],n)- functions dx/dt   (supplied by a user)
+ input ...
+ ti    - initial time
+ tf    - solution time
+ xi[]  - initial values
+ n     - number of first order equations
+ output ...
+ xf[]  - solutions
+==========================================================*/
+double rkv4(double(dnx)(double, double [], double [], int),
+               double ti, double tf, double ri[], double rf[], int n)
+{
+      double h, t, r[n], dr[n];
+      double k1[n],k2[n],k3[n],k4[n];
+      int j;
+
+      h = tf-ti;
+      t = ti;
+//k1
+      dnx(t, ri, dr, n);
+      for (j = 0; j<=n-1; j = j+1)
+        {
+          k1[j] = h*dr[j];
+          r[j]  = ri[j] + k1[j]/2.0;
+        }
+//k2
+      dnx(t+h/2.0, r, dr, n);
+      for (j = 0; j<=n-1; j = j+1)
+        {
+          k2[j] = h*dr[j];
+          r[j]  = ri[j] + k2[j]/2.0;
+        }
+//k3
+      dnx(t+h/2.0, r, dr, n);
+      for (j = 0; j<=n-1; j = j+1)
+        {
+          k3[j] = h*dr[j];
+          r[j]  = ri[j] + k3[j];
+        }
+//k4 and result
+      dnx(t+h, r, dr, n);
+      for (j = 0; j<=n-1; j = j+1)
+        {
+          k4[j] = h*dr[j];
+          rf[j] = ri[j] + k1[j]/6.0+k2[j]/3.0+k3[j]/3.0+k4[j]/6.0;
+        }
+    return 0.0;
+
+    // system ("gnuplot Plot.gp");
+}
+
+
+
---
+++ Flugbahn.h
@@ -0,0 +1,114 @@
+/*
+ * Flugbahn.h
+ *
+ *  Created on: 16.07.2018
+ *      Author: ls
+ */
+
+#ifndef FLUGBAHN_H_
+#define FLUGBAHN_H_
+
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+
+
+using namespace std;				//für Ausgabe
+
+/* function prototypes */
+double dnx(double, double [], double [], int );
+double rkv4(double(*)(double, double [], double [], int ),
+               double, double, double [], double [], int);
+
+
+/* global variables */
+
+const double G = 6.67*pow(10, -11);             // Gravitationskonstante
+const double m = 455000;                		// Masse ISS
+const double M = 5.97*pow(10, 24);				//Masse Erde
+const double c_w = 0.6;							//Widerstandsbeiwert
+const double A = 2667.78;						//Querschnittsfläche ISS
+const double rho0 = 1.3;
+		//1.77*pow(10, -22);			//Dichte der Atmosphäre (Geschätzter Wert!!!!!!!!!)
+const double R = 6371000;						//Radius Erde
+const double H0 = 7900;					//Parameter in der barometrischen Höhenformel !!!!!!!!!!!!!????????????
+
+
+const double rad = 3.1415926/180.0;  // radians
+
+
+
+int main()
+{
+    const int n=4;                   // number of first-order equations
+    double ti, tf, dt, tmax;
+    double ri[n], rf[n];		//Array
+    double v0, phi0;
+    int i;						// , key;
+
+
+/* output: file and formats */
+    ofstream file;
+    file.open ("table01c.dat");         // Ergebnisse werden in dieses File geschrieben
+/* output format */
+    file.precision(3);
+    file.setf(ios::scientific | ios::showpoint);
+    cout.precision(6);
+    cout.setf(ios::fixed | ios::showpoint);
+
+
+
+
+/* initial information */
+    ti = 0.0;                // initial value for variable t
+    v0 = 7666.67;             	 // Geschwindigkeit (m/s)
+    phi0 =  0.0;             // initial angle (degrees)
+    ri[0] = 408000+R;        // Anfangsposition in x (m)
+    ri[1] = 0.0;             // Anfangsposition in y (m)
+    ri[2] = 0.0; 			  // Geschwindigkeit in x-Richtung (m.s)
+    ri[3] = v0; 				  // Geschwindigkeit in y-Richtung  (m/s)
+
+    dt = 10;             // step size for integration (s)
+    tmax = 170000;          // integrate till tmax (s)
+/* end of initial information */
+
+    file << setw(12) << "t"  << setw(12) << "x"   << setw(12) << "y"
+         << setw(12) << "x'" << setw(12) << "y'"  << endl;
+
+/* integration of ODE */
+    while (ti <= tmax)
+    {
+
+     file << setw(12) << ti   << setw(12) << ri[0] << setw(12) << ri[1]
+          << setw(12) << ri[2]<< setw(12) << ri[3] << endl;
+
+     //if (sqrt(pow(ri[0], 2)+pow(ri[1],2)) < R) break;
+
+     tf = ti + dt;
+/*=================================*/
+     rkv4(dnx, ti, tf, ri, rf, n);
+/*=================================*/
+
+// prepare for the next step
+        ti = tf;
+        for (i = 0; i<=n-1; i = i+1)
+        {
+           ri[i] = rf[i];
+        }
+   }
+
+    // system ("read");
+
+
+
+    return 0;
+}
+
+
+
+
+#endif /* FLUGBAHN_H_ */
